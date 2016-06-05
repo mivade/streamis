0b7f3e2d42d03273a7b3007c8313540a43a29178
Michael V. DePalatis <mike@depalatis.net>
2016-06-05 18:06:44 +0200

    Add ability to broadcast to all listeners

--- streamis.py
+++ streamis.py
@@ -1,7 +1,9 @@
 import logging
 import asyncio
+from asyncio import Queue
 from tornado.platform.asyncio import AsyncIOMainLoop
 from tornado import web, log
+from tornado.iostream import StreamClosedError
 import aioredis
 
 AsyncIOMainLoop().install()
@@ -34,37 +36,58 @@ class Subscription:
     def __init__(self, redis, channel: str):
         self._redis = redis
         self.name = channel
+        self.listeners = set()
 
     async def subscribe(self):
-        self.channel, = await self._redis.subscribe(self.name)
+        res = await self._redis.subscribe(self.name)
+        self.channel = res[0]
 
     def __str__(self):
         return self.name
 
-    async def get(self):
-        """Return the next message as it comes in."""
-        msg = await self.channel.get(encoding='utf-8')
-        return msg
+    def add_listener(self, listener):
+        self.listeners.add(listener)
+
+    async def broadcast(self):
+        """Listen for new messages on Redis and broadcast to all
+        HTTP listeners.
+
+        """
+        while len(self.listeners) > 0:
+            msg = await self.channel.get()
+            logger.debug("Got message: %s" % msg)
+            closed = []
+            for listener in self.listeners:
+                try:
+                    listener.queue.put_nowait(msg)
+                except:
+                    logger.warning('Message delivery failed. Client disconnection?')
+                    closed.append(listener)
+            if len(closed) > 0:
+                [self.listeners.remove(listener) for listener in closed]
 
 
 class SubscriptionManager:
     """Manages all subscriptions."""
-    def __init__(self, redis_config=DEFAULT_CONFIG):
+    def __init__(self, redis_config=DEFAULT_CONFIG, loop=None):
         self.redis = None
         self.redis_config = redis_config
         self.subscriptions = dict()
+        self.loop = loop or asyncio.get_event_loop()
 
     async def connect(self):
         self.redis = await Connection.redis(self.redis_config)
 
-    async def subscribe(self, channel: str):
+    async def subscribe(self, listener, channel: str):
         """Subscribe to a new channel."""
         if channel in self.subscriptions:
-            return self.subscriptions[channel]
-        subscription = Subscription(self.redis, channel)
-        await subscription.subscribe()
-        self.subscriptions[channel] = subscription
-        return subscription
+            subscription = self.subscriptions[channel]
+        else:
+            subscription = Subscription(self.redis, channel)
+            await subscription.subscribe()
+            self.subscriptions[channel] = subscription
+            self.loop.call_soon(lambda: asyncio.Task(subscription.broadcast()))
+        subscription.add_listener(listener)
 
     def unsubscribe(self, channel: str):
         """Unsubscribe from a channel."""
@@ -77,12 +100,20 @@ class SubscriptionManager:
 
 class SSEHandler(web.RequestHandler):
     def initialize(self, manager: SubscriptionManager):
+        self.queue = Queue()
         self.manager = manager
+        self.set_header('content-type', 'text/event-stream')
+        self.set_header('cache-control', 'no-cache')
 
     async def get(self, channel: str):
-        subscription = await self.manager.subscribe(channel)
-        message = await subscription.get()
-        self.write(message)
+        await self.manager.subscribe(self, channel)
+        while True:
+            message = await self.queue.get()
+            try:
+                self.write("data: %s\n\n" % message)
+                await self.flush()
+            except StreamClosedError:
+                break
 
 
 if __name__ == "__main__":
